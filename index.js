dc8a53b9e5b8487dd47ff11ab2ab4eb436df65c1
jkroso <jkroso@gmail.com>
2013-05-09 16:39:37 +1200

    fix error handling

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-if (typeof setImmediate === 'function') {
+if (typeof setImmediate == 'function') {
   module.exports = setImmediate
 }
-// handle node.js
-else if (typeof process !== 'undefined' && process && typeof process.nextTick === 'function') {
-  module.exports = function(fn){ return process.nextTick(fn) };
+// legacy node.js
+else if (typeof process != 'undefined' && typeof process.nextTick == 'function') {
+  module.exports = process.nextTick
 }
 // fallback for other environments / postMessage behaves badly on IE8
-else if (typeof window === 'undefined' || window.ActiveXObject || !window.postMessage) {
+else if (typeof window == 'undefined' || window.ActiveXObject || !window.postMessage) {
   module.exports = setTimeout;
 } else {
   var q = [];
 
-  window.addEventListener('message', function f(e){
+  window.addEventListener('message', function(){
     var i = 0;
     while (i < q.length) {
       try { q[i++](); }
-      catch (err) {
-        q = q.slice(i + 1);
-        post();
-        throw err;
+      catch (e) {
+        q = q.slice(i);
+        window.postMessage('tic!', '*');
+        throw e;
       }
     }
     q.length = 0;
   }, true);
 
   module.exports = function(fn){
-    if (!q.length) post();
+    if (!q.length) window.postMessage('tic!', '*');
     q.push(fn);
   }
-
-  var post = function() { window.postMessage('tic!', '*'); }
 }
